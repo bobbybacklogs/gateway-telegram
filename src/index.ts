5a78cd6e75eef3378856f4d1ec8e11bc7eedb46a
Bobby Backlogs <bobbybacklogs@gmail.com>
2026-09-14 11:00:38 -0700

    Add package barrel exports.

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+export {
+  GatewayTelegramAdapter,
+  telegramOffered,
+  TELEGRAM_SUBAGENT_CAP,
+  TELEGRAM_A2A_DEPTH_CAP,
+} from "./adapter.js";
+export type {
+  GatewayTelegramAdapterOptions,
+  OrchestrateChatFn,
+} from "./adapter.js";
+export { createTelegramWebhookHandler } from "./webhook.js";
+export {
+  TelegramBotApi,
+  TELEGRAM_TEXT_LIMIT,
+  TELEGRAM_CALLBACK_DATA_LIMIT,
+  escapeTelegramHtml,
+  splitTelegramText,
+  telegramPersonaFooter,
+  withPersonaFooter,
+  telegramWebhookSecretMatches,
+} from "./telegram-api.js";
+export type { TelegramUpdate } from "./telegram-api.js";
+export { parseTelegramCommand } from "./commands.js";
+export {
+  MemoryHitlStore,
+  parseTelegramCallbackData,
+  telegramCallbackData,
+} from "./hitl.js";
+export { consumeAgentSse } from "./sse.js";
+export { handleTelegramUpdate } from "./handle.js";
